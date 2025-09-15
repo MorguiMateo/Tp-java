a3dcfa937692a223a48fc89b40cc2aa5bb579938
MorguiMateo <148649344+MorguiMateo@users.noreply.github.com>
2025-09-15 19:50:05 -0300

    Add files via upload

---
+++ ReflectionTp2/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ReflectionExample reflectionExample = new ReflectionExample();
+        reflectionExample.ejecutarReflexion();
+    }
+}
